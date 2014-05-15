a665be4fa8924bbc4bf0ccedf65191fb6f7d576b
Friso Abcouwer <f.abcouwer@gmail.com>
2014-05-15 16:43:16 +0200

    SAXParser and typo in xml testfile

--- assignment-1/code/src/TestParse.java
+++ assignment-1/code/src/TestParse.java
@@ -1,18 +1,26 @@
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.ContentHandler;
 import org.xml.sax.XMLReader;
-
 
 public class TestParse {
 
 	public static void main(String[] args) {
 		try {
-		      XMLReader parser = org.xml.sax.helpers.XMLReaderFactory.createXMLReader();
-		      ContentHandler contentHandler = new StackEval();
-		      parser.setContentHandler(contentHandler);
-		      parser.parse("test.xml");
-		    } 
-		catch (Exception e) {
-		      e.printStackTrace();
-		    }
+
+			//Initialize SAX XMLReader
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser parser = factory.newSAXParser();
+			XMLReader reader = parser.getXMLReader();
+			
+			//Link StackEval to that reader and read the xml file
+			ContentHandler contentHandler = new StackEval();
+			reader.setContentHandler(contentHandler);
+			reader.parse("test.xml");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
