5c2ac64e3f7b2c56a3a10170dee57d4fc1dbf922
Friso Abcouwer <f.abcouwer@gmail.com>
2014-05-29 14:32:35 +0200

    Parse For/In of a query

--- assignment-1/code/src/InputHandler.java
+++ assignment-1/code/src/InputHandler.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 //InputHandler: construct tree from a given query
@@ -10,7 +11,11 @@ public class InputHandler {
 	private String qReturn = "";
 
 	private PatternNode root;
-	private HashMap<String, PatternNode> nodes;
+	private HashMap<String, PatternNode> nodes = new HashMap<String, PatternNode>();
+	private HashMap<String, TPEStack> stacks = new HashMap<String, TPEStack>();
+	private HashMap<String, String> parents = new HashMap<String, String>();
+
+	// private ArrayList<String> requiredNodes = new ArrayList<String>();
 
 	public InputHandler(String q) {
 		this.query = q;
@@ -47,7 +52,6 @@ public class InputHandler {
 
 	private void parseIn() {
 		String content = qIn.substring(3);
-		System.out.println(content);
 
 		int delimiter = content.indexOf("[");
 		String[] descendants = new String[0];
@@ -59,16 +63,51 @@ public class InputHandler {
 			root = new PatternNode(content.substring(2, delimiter));
 			descendants = content.substring(delimiter).split("]");
 		}
+		TPEStack rootStack = new TPEStack(root, null);
+		stacks.put(root.getName(), rootStack);
+		nodes.put(root.getName(), root);
 
 		for (int i = 0; i < descendants.length; i++) {
 			// Remove the [ from descendant names
 			descendants[i] = descendants[i].substring(1);
-			System.out.println(descendants[i]);
-			
-			//TODO parse descendants for new nodes
-		}
-		System.out.println();
+			// System.out.println(descendants[i]);
 
+			// parse descendants for new nodes
+			parseDescendants(root, descendants[i]);
+		}
+
+	}
+
+	private void parseDescendants(PatternNode parent, String remaining) {
+		if (remaining.startsWith("//")) {
+			// TODO handle ancestors
+		} else {
+			// check if we need to go deeper
+			int loc = remaining.indexOf("/");
+			PatternNode newNode;
+
+			if (loc > 0) {
+				newNode = new PatternNode(remaining.substring(0, loc));
+			} else {
+				newNode = new PatternNode(remaining);
+			}
+
+			if (newNode.getName().equals("*")) {
+				newNode.setWildcard(true);
+			}
+			nodes.put(newNode.getName(), newNode);
+
+			TPEStack nodeStack = new TPEStack(newNode, stacks.get(parent
+					.getName()));
+			stacks.put(newNode.getName(), nodeStack);
+			stacks.get(parent.getName()).addChildStack(nodeStack);
+			parent.addChild(newNode);
+			parents.put(parent.getName(), newNode.getName());
+
+			if (loc > 0)
+				parseDescendants(newNode, remaining.substring(loc + 1));
+
+		}
 	}
 
 	private void parseWhere() {
@@ -81,8 +120,13 @@ public class InputHandler {
 
 	}
 
+	// Link all parent-child pairs together
+	public void processParents() {
+		// TODO
+	}
+
 	public static void main(String[] args) {
-		String testQ = "for $p in //person[//first][//last] where $p/email=’m@home’ return ($p//first, $p//last)";
+		String testQ = "for $p in //person[email][name/last] where $p/email=’m@home’ return ($p//first, $p//last)";
 
 		InputHandler ih = new InputHandler(testQ);
 		System.out.println(ih.qFor);
